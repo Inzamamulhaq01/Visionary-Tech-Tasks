f1254629673609bfd862fbc95c6227d897faf42a
Inzamamulhaq01 <156565451+Inzamamulhaq01@users.noreply.github.com>
2024-12-20 17:44:56 +0530

    Update conditions.php

--- conditions.php
+++ conditions.php
@@ -3,9 +3,9 @@
 $age = 20;
 
 if ($age >= 18) {
-    echo "You are an adult.";
+    echo "<h3>You are an adult.</h3>";
 } else {
-    echo "You are a minor.";
+    echo "<h3>You are a minor.</h3>";
 }
 
 echo "<br>";
@@ -13,9 +13,9 @@ echo "<br>";
 $number = "abc";
 
 if (!is_numeric($number)) {
-    echo "The input is not a valid number.";
+    echo "<h3>The input is not a valid number.</h3>";
 } else {
-    echo "The input is a valid number.";
+    echo "<h3>The input is a valid number.</h3>";
 }
 echo "<br>";
 
@@ -23,9 +23,9 @@ echo "<br>";
 $file = "path/to/file.txt";
 
 if (!file_exists($file)) {
-    echo "File does not exist.";
+    echo "<h3>File does not exist.</h3>";
 } else {
-    echo "File found.";
+    echo "<h3>File found.</h3>";
 }
 
 
@@ -35,7 +35,7 @@ $x = 10;
 
 if ($x > 5) 
 {  
-    echo "X is greater than 5";
+    echo "<h3>X is greater than 5</h3>";
 }
 
 echo "<br>";
@@ -43,39 +43,39 @@ echo "<br>";
 $x = 3;
 
 if ($x) {  
-    echo "X is non-zero";
+    echo "<h3>X is non-zero</h3>";
 }
 
 echo "<br>";
 
 $age = 65;
 if ($age >= 18) {
-    echo "You are eligible to vote.<br>";
+    echo "<h3>You are eligible to vote.<br></h3>";
 
     if ($age >= 60) {
-        echo "You are eligible for a senior citizen discount.";
+        echo "<h3>You are eligible for a senior citizen discount.</h3>";
     } else {
-        echo "You are not eligible for a senior citizen discount.";
+        echo "<h3>You are not eligible for a senior citizen discount.</h3>";
     }
 } else {
-    echo "You are not eligible to vote.";
+    echo "<h3>You are not eligible to vote.</h3>";
 }
 
 
 $score = 85;
 
 if ($score >= 50) {
-    echo "You passed the exam.<br>";
+    echo "<h3>You passed the exam.<br></h3>";
 
     if ($score >= 90) {
-        echo "Excellent performance! You get a bonus.";
+        echo "<h3>Excellent performance! You get a bonus.</h3>";
     } elseif ($score >= 75) {
-        echo "Good performance!";
+        echo "<h3>Good performance!</h3>";
     } else {
-        echo "You passed, but there's room for improvement.";
+        echo "<h3>You passed, but there's room for improvement.</h3>";
     }
 } else {
-    echo "You failed the exam.";
+    echo "<h3>You failed the exam.</h3>";
 }
 
 ?>
